3fd10c5fd62ae28ae2e5199b522260f018ea6db8
noamlew <65733811+noamlew@users.noreply.github.com>
2020-09-10 18:22:38 +0300

    Add files via upload

---
+++ all games.js
@@ -0,0 +1,39 @@
+function Snake() {
+    globalThis.location = "https://noamlew.github.io/noams-Snake/.";
+}
+
+function PingPong() {
+    globalThis.location = "https://noamlew.github.io/ping-pong/.";
+}
+
+function Cactus() {
+    globalThis.location = "https://noamlew.github.io/noam-s-cactus-play/.";
+}
+
+function Ritter() {
+    globalThis.location = "https://noamlew.github.io/Ritter/.";
+}
+
+function Jang() {
+    globalThis.location = "https://noamlew.github.io/Jang-game/.";
+}
+
+function FarBy() {
+    globalThis.location = "https://noamlew.github.io/FarBy/.";
+}
+
+function snake2() {
+    globalThis.location = "https://noamlew.github.io/good-snake-game/";
+}
+
+function memory() {
+    globalThis.location = "https://noamlew.github.io/memory/.";
+}
+
+function Adeilad() {
+    globalThis.location = "https://noamlew.github.io/Adeiled-game/";
+}
+
+function yashlatz() {
+    globalThis.location = "https://noamlew.github.io/yashlatznik/.";
+}
